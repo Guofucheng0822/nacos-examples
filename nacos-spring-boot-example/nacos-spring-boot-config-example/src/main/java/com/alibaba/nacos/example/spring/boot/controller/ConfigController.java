5d4e44640bd93db8566c23fbc0ea890c697e0ca8
““guofucheng <guofucheng@e6yun.com>
2021-03-03 09:54:25 +0800

    first commit

---
+++ nacos-spring-boot-example/nacos-spring-boot-config-example/src/main/java/com/alibaba/nacos/example/spring/boot/controller/ConfigController.java
@@ -0,0 +1,28 @@
+package com.alibaba.nacos.example.spring.boot.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * nacos配置测试控制器
+ * http://127.0.0.1:8080/config/get
+ * @author guofucheng
+ * @date 2021/03/01
+ */
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+}
